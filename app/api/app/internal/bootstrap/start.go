786b983c2d33be1ae0b4a79b54a0600f6820e9cd
sh7ning <sh7ning@qq.com>
2020-12-22 21:57:07 +0800

    wait signal

--- app/api/app/internal/bootstrap/start.go
+++ app/api/app/internal/bootstrap/start.go
@@ -4,13 +4,9 @@ import (
 	"codebase/app/api/app/internal/global"
 	"codebase/app/api/app/internal/web"
 	"codebase/pkg/defers"
-	"codebase/pkg/log"
-	"os"
-	"os/signal"
-	"syscall"
+	"codebase/pkg/signal"
 
 	"github.com/spf13/pflag"
-	"go.uber.org/zap"
 )
 
 func Start(cfgFile string, flagSet *pflag.FlagSet) {
@@ -23,14 +19,5 @@ func Start(cfgFile string, flagSet *pflag.FlagSet) {
 	web.New()
 
 	// Wait for interrupt signal to gracefully shutdown the server with
-	// a timeout of 5 seconds.
-	quit := make(chan os.Signal)
-	// kill (no param) default send syscall.SIGTERM
-	// kill -2 is syscall.SIGINT
-	// kill -9 is syscall.SIGKILL but can't be catch, so don't need add it
-	//register for interupt (Ctrl+C) and SIGTERM (docker)
-	//todo 增加平滑重启信号相关处理逻辑
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
-	sig := <-quit
-	log.Warn("get signal, start shutdown server ...", zap.Any("signal", sig))
+	signal.Wait()
 }
