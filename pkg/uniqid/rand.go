7a8a7372570285ada1d808e1037abc0169a319e4
sh7ning <sh7ning@qq.com>
2021-02-06 20:11:53 +0800

    d

--- pkg/uniqid/rand.go
+++ pkg/uniqid/rand.go
@@ -12,7 +12,7 @@ func init() {
 	randInstance = rand.New(rand.NewSource(time.Now().UnixNano()))
 }
 
-//// GenerateID simply generates an ID.
+// GenerateID simply generates an ID.
 func GenerateID() string {
 	return fmt.Sprintf("%016x", uint64(randInstance.Int63()))
 }
